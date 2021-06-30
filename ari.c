230fbf4c30121314bd66bf5ce3a9b4bc741fabc6
Sreenivas360 <85107140+Sreenivas360@users.noreply.github.com>
2021-06-30 13:03:26 +0530

    Sree

--- ari.c
+++ ari.c
@@ -1,58 +1,76 @@
-#include<stdio.h>
-#include <assert.h>
-#include <string.h>
-#include <cs50.h>
-#include <ctype.h>
 
-string ari(string s){
-	int i = 0;
-	float t = 0;
-	float count1 = 0;
-	float count2 = 0;
-	float count3 = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (isalnum(s[i]))
-			count1++;
-		if (s[i] == ' ')
-			count2++;
-		if (s[i] == '.' || s[i] == '?' || s[i] == '!')
-			count3++;
-	}
-	t = 4.71 * (count1 / count2) + 0.5 * (count2 / count3) - 21.43;
-	int y = t + 1;
-	switch (y)
-	{
-	case 1:
-		return "Kindergarten";
-break;
-	case 2:
-		return "First/Second Grade";
-	case 3:
-		return "Third Grade";
-	case 4:
-		return "Fourth Grade";
-	case 5:
-		return "Fifth Grade";
-	case 6:
-		return "Sixth Grade";
-	case 7:
-		return "Seventh Grade";
-	case 8:
-		return "Eighth Grade";
-	case 9:
-		return "Ninth Grade";
-	case 10:
-		return "Tenth Grade";
-	case 11:
-		return "Eleventh Grade";
-	case 12:
-		return "Twelfth grade";
-	case 13:
-		return "College student";
-	case 14:
-		return "Professor";
-	default:
-		break;
-	}
+string ari(string s) {
+  
+  string grade;
+  float chara = 0, word = 0, sentence = 0;
+  for(int i = 0; i<strlen(s); i++) {
+    if(isalnum(s[i])) {
+      chara = chara + 1;
+    } 
+    if(s[i]==' ') {
+      word = word +1; 
+    }
+    if(s[i]=='.' || s[i]=='?' || s[i]=='!') {
+      sentence = sentence + 1;
+    }
+  }
+  float num1 = 4.71 * (chara/word);
+  float num2 = 0.5 * (word/sentence);
+  float num = num1 + num2 - 21.43;
+   // printf("%f\n", num);//
+  int b = 0;
+  
+  if(num==(int)num) {
+     b = (int)num;
+  }
+  else if(num>(int)num) {
+     b = (int)num + 1;
+  }
+
+  //printf("%d\n", b);//
+
+  if(b==1) {
+    grade = "Kindergarten";
+  }
+  if(b==2) {
+    grade="First/Second Grade";
+  }
+  if(b==3) {
+    grade="Third Grade";
+  }
+  if(b==4) {
+    grade="Fourth Grade";
+  }
+  if(b==5) {
+    grade="Fifth Grade";
+  }
+  if(b==6) {
+    grade="Sixth Grade";
+  }
+  if(b==7) {
+    grade="Seventh Grade";
+  }
+  if(b==8) {
+    grade="Eighth Grade";
+  }
+  if(b==9) {
+    grade="Ninth Grade";
+  }
+  if(b==10) {
+    grade="Tenth Grade";
+  }
+  if(b==11) {
+    grade="Eleventh Grade";
+  }
+  if(b==12) {
+    grade="Twelfth Grade";
+  }
+  if(b==13) {
+    grade="College Student";
+  }
+  if(b==14) {
+    grade="Professional";
+  }
+  //printf("%s\n", grade);//
+  return grade;
 }
