a8479effb60ed6fc19ddd84b0c2a98e8cd068f69
Sreenivas360 <85107140+Sreenivas360@users.noreply.github.com>
2021-06-30 11:49:44 +0530

    Sree

--- ari.c
+++ ari.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <cs50.h>
-#include<ctype.h>
-string ari(string s)
-{
+#include <ctype.h>
+
+string ari(string s){
 	int i = 0;
 	float t = 0;
 	float count1 = 0;
@@ -25,50 +25,33 @@ string ari(string s)
 	{
 	case 1:
 		return "Kindergarten";
-                break;
 	case 2:
 		return "First/Second Grade";
-                break;
 	case 3:
 		return "Third Grade";
-                break;
 	case 4:
 		return "Fourth Grade";
-                 break;
 	case 5:
 		return "Fifth Grade";
-                break;
 	case 6:
 		return "Sixth Grade";
-                break;
 	case 7:
 		return "Seventh Grade";
-                 break;
 	case 8:
 		return "Eighth Grade";
-                break;
 	case 9:
 		return "Ninth Grade";
-                 break;
 	case 10:
 		return "Tenth Grade";
-                break;
 	case 11:
 		return "Eleventh Grade";
-                break;
 	case 12:
 		return "Twelfth grade";
-                break;
 	case 13:
 		return "College student";
-                break;
 	case 14:
 		return "Professor";
-
-                break;
 	default:
 		break;
 	}
-	return ' ';
-
 }
