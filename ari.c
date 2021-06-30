09db4dba09b55f78493c3852eebb53cd5b058017
Sreenivas360 <85107140+Sreenivas360@users.noreply.github.com>
2021-06-30 21:19:14 +0530

    Solution

--- ari.c
+++ ari.c
@@ -1,57 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <cs50.h>
-#include <ctype.h>
 
-string ari(string s){
-	int i = 0;
-	float t = 0;
-	float count1 = 0;
-	float count2 = 0;
-	float count3 = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (isalnum(s[i]))
-			count1++;
-		if (s[i] == ' ')
-			count2++;
-		if (s[i] == '.' || s[i] == '?' || s[i] == '!')
-			count3++;
-	}
-	t = 4.71 * (count1 / count2) + 0.5 * (count2 / count3) - 21.43;
-	int y = t + 1;
-	switch (y)
-	{
-	case 1:
-		return "Kindergarten";
-	case 2:
-		return "First/Second Grade";
-	case 3:
-		return "Third Grade";
-	case 4:
-		return "Fourth Grade";
-	case 5:
-		return "Fifth Grade";
-	case 6:
-		return "Sixth Grade";
-	case 7:
-		return "Seventh Grade";
-	case 8:
-		return "Eighth Grade";
-	case 9:
-		return "Ninth Grade";
-	case 10:
-		return "Tenth Grade";
-	case 11:
-		return "Eleventh Grade";
-	case 12:
-		return "Twelfth grade";
-	case 13:
-		return "College student";
-	case 14:
-		return "Professor";
-	default:
-		break;
-	}
+string ari(string);
+
+string grade (int);
+
+string ari(string b)
+{
+ 
+  int w=0,s=0,k=0;
+ for(int i=0;i<strlen(b);i++)
+ {
+   
+   if(b[i]>='0' && b[i]<='9')
+    k++;
+   else if(b[i]>='a' && b[i]<='z')
+    k++;
+   else if(b[i]>='A' && b[i]<='Z')
+    k++;
+    
+   if(b[i]==' ')
+    w++;
+   else if(b[i]=='.' || b[i]=='?')
+    s++;
+    else if(b[i]=='!')
+    s++;
+   
+ }
+   
+  float add = (4.71*(float)(k)/w)+(0.5*((float)(w)/s))-21.43;
+  string grade(int add)
+{
+    
+    
+
+
+  switch(add){
+  case  1  : return"Kindergarten";
+  
+ case 2 :return "First/Second Grade";
+ case   3  : return "Third Grade";
+  case 4 : return "Fourth Grade";
+case     5 : return " Fifth Grade";
+ case 6 : return "Sixth Grade";
+   case 7  : return "Seventh Grade";
+    case  8 : return "Eighth Grade";
+    case  9 : return "Ninth Grade";
+  case  10 : return "Tenth Grade";
+   case 11 : return "Eleventh Grade";
+ case   12 : return "Twelfth grade";
+  case 13  : return "College student";
+  case  14: return "Professor";
+  
+ 
+    
+    }
+    }
+  b= grade((int)(add)+1);
+  
+  return b;
 }
